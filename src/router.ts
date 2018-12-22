40f5420573610413b9bfb975866645c6527f24da
yunik1004 <inkyuhak@gmail.com>
2018-12-23 00:16:15 +0900

    Modify router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import NotFound404 from './views/404.vue';
 import Test from './views/Test.vue';
 
 Vue.use(Router);
+
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+// component: () => import('./views/About.vue'),
 
 export default new Router({
   routes: [
@@ -18,12 +24,9 @@ export default new Router({
       component: Test,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '*',
+      name: '404',
+      component: NotFound404,
     },
   ],
 });
---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="404">
+    <h1>Page Not Found</h1>
+    <div id="nav">
+      <router-link to="/">Return to home</router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
